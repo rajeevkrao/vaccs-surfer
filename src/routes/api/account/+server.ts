f90b530d0fa74c5438dab228d61638e6b8660604
Rajeev K Rao <rajeevraok@gmail.com>
2026-01-27 11:51:29 +0530

    feat: enhance GET handler to support account data retrieval by name and tag

--- src/routes/api/account/+server.ts
+++ src/routes/api/account/+server.ts
@@ -1,14 +1,19 @@
-import { getAccountByPuuid } from '$lib/server/middleware';
+import { getAccountByNameTag, getAccountByPuuid } from '$lib/server/middleware';
 import type { RequestHandler } from '@sveltejs/kit';
 import axios from 'axios';
 
 export const GET: RequestHandler = async ({ url }) => {
 	try {
-		const puuid = url.searchParams.get('puuid') as string;
-		if (!puuid) {
-			return new Response('Bad Request: puuid is required', { status: 400 });
+		const puuid = url.searchParams.get('puuid');
+		const name = url.searchParams.get('name');
+		const tag = url.searchParams.get('tag');
+
+		if (!puuid && !(name && tag)) {
+			return new Response('Bad Request: puuid or (name and tag) is required', { status: 400 });
 		}
-		const data = await getAccountByPuuid(puuid);
+
+		const data = puuid ? await getAccountByPuuid(puuid) : await getAccountByNameTag(name!, tag!);
+
 		return Response.json(data);
 	} catch (err) {
 		if (axios.isAxiosError(err)) console.log(err?.response?.data);
