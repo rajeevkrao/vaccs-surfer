8b4af88fd9b2435a85b241099bd97d306a384670
Rajeev K Rao <rajeevraok@gmail.com>
2026-03-28 00:40:01 +0530

    feat: add logging functions for cluster count, total matches, and headshot percentage

---
+++ src/lib/loggers.ts
@@ -0,0 +1,32 @@
+export const logClusterCount = (matchesData: any) => {
+	let clusterCount: any = {};
+	matchesData.forEach((match: any) => {
+		clusterCount[match.meta.cluster] = (clusterCount[match.meta.cluster] || 0) + 1;
+	});
+	Object.entries(clusterCount).forEach(([cluster, count]) => {
+		console.log(`${cluster}: ${count}`);
+	});
+};
+
+export const logTotalMatches = (matchesdata: any) => {
+	// TODO: This is not including missing matches so implement for missing matches as well
+	console.log(`Total matches: ${matchesdata.length}`);
+};
+
+export const logHsPercentage = (matchesdata: any) => {
+	const totalShots = matchesdata.reduce((acc: number, match: any) => {
+		const head = Number(match?.stats?.shots?.head) || 0;
+		const body = Number(match?.stats?.shots?.body) || 0;
+		const legs = Number(match?.stats?.shots?.leg) || 0;
+
+		return acc + head + body + legs;
+	}, 0);
+
+	const headShots = matchesdata.reduce((acc: number, match: any) => {
+		const head = Number(match?.stats?.shots?.head) || 0;
+		return acc + head;
+	}, 0);
+
+	const hsPercentage = totalShots > 0 ? (headShots / totalShots) * 100 : 0;
+	console.log(`Head shot percentage: ${hsPercentage.toFixed(2)}%`);
+};
--- src/routes/matchesv2/[name]/[tag]/+page.svelte
+++ src/routes/matchesv2/[name]/[tag]/+page.svelte
@@ -2,9 +2,15 @@
 	import MatchList from '$lib/components/MatchList.svelte';
 	import PlayerData from '$lib/components/PlayerData.svelte';
 	import ScrollToTop from '$lib/components/ScrollToTop.svelte';
+	import { logClusterCount, logHsPercentage, logTotalMatches } from '$lib/loggers';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+	$effect(() => {
+		logClusterCount(data.matchesData);
+		logTotalMatches(data.matchesData);
+		logHsPercentage(data.matchesData);
+	});
 </script>
 
 <div class="bg-[#333]">
